036b9053fafd5ce8589c63b595f3a79523e5282f
priyadharshinitc <priyadharshinihp@gmail.com>
2024-11-21 12:11:37 +0530

    Updated Greenden with JS

--- Greenden/script.js
+++ Greenden/script.js
@@ -1,6 +1,10 @@
 let menuicon = document.getElementById("menuicon");
 let sidenavbar = document.getElementById("sidenavbar");
 let closeicon = document.getElementById("closeicon");
+let watchVideo = document.getElementById("watch-video");
+let videoContainer = document.getElementById("video-container");
+let video = document.getElementById("video");
+let closeButton = document.getElementById("close");
 
 menuicon.addEventListener("click", function() {
     sidenavbar.style.right = "0";
@@ -8,4 +12,14 @@ menuicon.addEventListener("click", function() {
 
 closeicon.addEventListener("click", function() {
     sidenavbar.style.right = "-50%";
-})
+});
+
+watchVideo.addEventListener("click", function() {
+    videoContainer.style.display = "flex";
+});
+
+closeButton.addEventListener("click", function() {
+    video.pause();
+    video.currentTime = 0;
+    videoContainer.style.display = "none";
+});

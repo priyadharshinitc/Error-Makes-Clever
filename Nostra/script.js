f79349a04522300e5271d8719bb129ad5fd30d88
priyadharshinitc <priyadharshinihp@gmail.com>
2024-11-23 22:49:17 +0530

    Added Collections section for Mobile Small

--- Nostra/script.js
+++ Nostra/script.js
@@ -2,9 +2,11 @@
 let offerCloseIcon = document.getElementById("offerCloseIcon");
 let offerBanner = document.querySelector(".offerBanner");
 
-offerCloseIcon.addEventListener("click", function() {
-    offerBanner.style.display = "none";
-});
+if(offerCloseIcon) {
+    offerCloseIcon.addEventListener("click", function() {
+        offerBanner.style.display = "none";
+    });
+}
 
 // Side Navbar Close Functionality
 let menubar = document.getElementById("menubar");
@@ -24,15 +26,19 @@ let sliderImgContainer = document.querySelector(".slider__imgContainer");
 let sliderLeftArrow = document.getElementById("sliderLeftArrow");
 let sliderRightArrow = document.getElementById("sliderRightArrow");
 
-sliderLeftArrow.addEventListener("click", function() {
-    sliderImgContainer.style.scrollBehavior = "smooth";
-    sliderImgContainer.scrollLeft -= 300; 
-});
+if(sliderLeftArrow) {
+    sliderLeftArrow.addEventListener("click", function() {
+        sliderImgContainer.style.scrollBehavior = "smooth";
+        sliderImgContainer.scrollLeft -= 300; 
+    });
+}
 
-sliderRightArrow.addEventListener("click", function() {
-    sliderImgContainer.style.scrollBehavior = "smooth";
-    sliderImgContainer.scrollLeft += 300; 
-});
+if(sliderRightArrow) {
+    sliderRightArrow.addEventListener("click", function() {
+        sliderImgContainer.style.scrollBehavior = "smooth";
+        sliderImgContainer.scrollLeft += 300; 
+    });
+}
 
 // Save favourite collections using save button
 let savedLists = document.querySelectorAll(".saveBtn");

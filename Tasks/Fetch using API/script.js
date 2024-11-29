a7e90c8c28a5fdfefa8300cdbdf318327796cf4e
priyadharshinitc <priyadharshinihp@gmail.com>
2024-11-29 20:30:11 +0530

    Fetch using API folder added

--- Tasks/Fetch using API/script.js
+++ Tasks/Fetch using API/script.js
@@ -1,6 +1,6 @@
 const fetchBtn = document.getElementById("fetchBtn");
-const inputBox = document.getElementById("inputBox");
 const filter = document.querySelector(".filter");
+const inputBox = document.getElementById("inputBox");
 const searchBtn = document.getElementById("searchBtn");
 const result = document.getElementById("result");
 let jsonUserData = [];
@@ -8,7 +8,6 @@ async function getUserData() {
     try {
         const response = await fetch("https://jsonplaceholder.typicode.com/posts");
         jsonUserData = await response.json();
-        // console.log(jsonUserData);
         jsonUserData.forEach((userData) => {
             result.innerHTML += `
                                     <li>
@@ -20,31 +19,35 @@ async function getUserData() {
         });
     } catch (error) {
         result.innerHTML = error.message + " 😟";
-        result.style.fontSize = "20px";
+        result.style.fontSize = "24px";
         result.style.textAlign = "center";
     }
 };
 
 function filteredData() {
     let userInput = inputBox.value.trim().toLowerCase();
-    result.innerHTML = "";
-    for(const userData of jsonUserData) {
-        if((userInput !== "") && userData.title.includes(userInput)) {
-            result.innerHTML += `
-                                <li>
-                                    <h2>User ID: ${userData.userId}</h2>
-                                    <h2>Title: ${userData.title}</h2>
-                                    <p><strong>Description: </strong>${userData.body}</p>
-                                </li>
-            `;
-        } else {
-            continue;
+    if(userInput === "") {
+        window.alert("Enter you search input");
+    } else {
+        result.innerHTML = "";
+        for(const userData of jsonUserData) {
+            if(userData.title.includes(userInput)) {
+                result.innerHTML += `
+                                    <li>
+                                        <h2>User ID: ${userData.userId}</h2>
+                                        <h2>Title: ${userData.title}</h2>
+                                        <p><strong>Description: </strong>${userData.body}</p>
+                                    </li>
+                `;
+            } else {
+                continue;
+            }
         }
-    }
+    }    
 }
 
 fetchBtn.addEventListener("click", getUserData);
-filter.addEventListener("submit", function(){
+filter.addEventListener("submit", function(event){
     event.preventDefault();
     filteredData;
 });

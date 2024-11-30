f3e90d04a6dc9504d88439944a5d06b2bc97bf44
priyadharshinitc <priyadharshinihp@gmail.com>
2024-11-30 15:36:19 +0530

    JavaScript written for Username, Email address validation

--- Quiz/script.js
+++ Quiz/script.js
@@ -8,3 +8,33 @@ let qna = document.getElementById("qna");
 //     instructions.style.display = "none";
 //     qna.style.display = "none";
 // }
+
+// Selection
+let nameRegex = /^[a-zA-Z]+(?: [a-zA-Z]+)*$/;
+let emailRegex = /^[a-zA-Z0-9]+@[a-z]+\.[a-z]+$/;
+
+let userName = document.getElementById("userName");
+let email = document.getElementById("email");
+
+let emptyBox = document.querySelectorAll(".emptyBox");
+let nameError = document.getElementById("nameError");
+let emailError = document.getElementById("emailError");
+
+// Checking Valid Username, email address
+userName.addEventListener("input", function() {
+    if(nameRegex.test(userName.value) === false) {
+        emptyBox[0].classList.add("hidden");
+        nameError.classList.remove("hidden");
+    } else {
+        nameError.classList.add("hidden");
+    }
+});
+
+email.addEventListener("input", function(){
+    if(emailRegex.test(email.value) === false) {
+        emptyBox[1].classList.add("hidden");
+        emailError.classList.remove("hidden");
+    } else {
+        emailError.classList.add("hidden");
+    }
+});

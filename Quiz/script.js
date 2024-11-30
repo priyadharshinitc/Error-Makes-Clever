07e528a2c421baa2e5b59274dbcbc743cee1b68a
priyadharshinitc <priyadharshinihp@gmail.com>
2024-11-30 15:35:17 +0530

    JavaScript written for Initial Display

---
+++ Quiz/script.js
@@ -0,0 +1,10 @@
+// Initial Display
+let login = document.getElementById("login");
+let instructions = document.getElementById("instructions");
+let qna = document.getElementById("qna");
+
+// window.onload = () => {
+//     login.style.display = "block";
+//     instructions.style.display = "none";
+//     qna.style.display = "none";
+// }
